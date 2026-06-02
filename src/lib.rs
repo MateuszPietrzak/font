18ccd1f56ff6cf99e8ee85ef8af78e868a183420
Mateusz Pietrzak <mateusz.jerzy.pietrzak@gmail.com>
2026-06-02 21:44:06 +0200

    Draw a triangle to the screen
    + Vertices hardcoded in the shader

--- src/lib.rs
+++ src/lib.rs
@@ -55,6 +55,52 @@ async fn start() {
 
     surface.configure(&device, &config);
 
+    let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+
+    let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: Some("Render Pipeline Layout"),
+        bind_group_layouts: &[],
+        immediate_size: 0,
+    });
+
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: Some("Render Pipeline"),
+        layout: Some(&render_pipeline_layout),
+        vertex: wgpu::VertexState {
+            module: &shader,
+            entry_point: Some("vs_main"),
+            buffers: &[],
+            compilation_options: wgpu::PipelineCompilationOptions::default(),
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: &shader,
+            entry_point: Some("fs_main"),
+            targets: &[Some(wgpu::ColorTargetState {
+                format: config.format,
+                blend: Some(wgpu::BlendState::REPLACE),
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+            compilation_options: wgpu::PipelineCompilationOptions::default(),
+        }),
+        primitive: wgpu::PrimitiveState {
+            topology: wgpu::PrimitiveTopology::TriangleList,
+            strip_index_format: None,
+            front_face: wgpu::FrontFace::Ccw,
+            cull_mode: Some(wgpu::Face::Back),
+            polygon_mode: wgpu::PolygonMode::Fill,
+            unclipped_depth: false,
+            conservative: false,
+        },
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState {
+            count: 1,
+            mask: !0,
+            alpha_to_coverage_enabled: false,
+        },
+        multiview_mask: None,
+        cache: None,
+    });
+
     let output = surface.get_current_texture().unwrap();
 
     let view = output
@@ -65,7 +111,7 @@ async fn start() {
         label: Some("Render Encoder"),
     });
 
-    let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+    let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
         label: Some("Render Pass"),
         color_attachments: &[Some(wgpu::RenderPassColorAttachment {
             view: &view,
@@ -87,7 +133,10 @@ async fn start() {
         multiview_mask: None,
     });
 
-    drop(_render_pass);
+    render_pass.set_pipeline(&render_pipeline);
+    render_pass.draw(0..3, 0..1);
+
+    drop(render_pass);
 
     queue.submit(std::iter::once(encoder.finish()));
     output.present();
